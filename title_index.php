2e0510098a7357cb8ec6519205c75b1a60b38fff
phareed3310 <fareedabdulsalam18@gmail.com>
2024-01-09 22:41:04 +0100

    complete project

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/fudd.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Federal University Dutse - Computer Science</p>
+							<h3>
+
+							<p>E - Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>FCP EXCELS:</p>
+												<p>Adaptive Immersive and Data Driven Education</p>
+												
+								</div>		
+						</div>		
+				</div>
